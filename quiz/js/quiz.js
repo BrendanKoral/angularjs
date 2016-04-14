7e8e657734aa1718db449dd7fd820052961085eb
Brendan Koral <brendanhkoral@gmail.com>
2016-04-13 22:07:22 -0500

    Set up quiz.js and JSON data, minor HTML
    Set up myQuiz app, QuizController controller

--- quiz/js/quiz.js
+++ quiz/js/quiz.js
@@ -1,5 +1,23 @@
 (function(){
 
-	// script goes here
+	var app = angular.module('myQuiz', []);
+
+    app.controller('QuizController',['$scope','$http','$sce',function($scope,$http,$sce){
+        
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        //Allows us to load the intro screen. Otherwise, it will load item 0 from array
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
+
+        $http.get('quiz_data.json').then(function(quizData){
+            //Puts quiz data in the quizData object
+            //Then store it in the variable called myQuestions
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length
+            //Create totalQuestions variable using length of myQuestions
+        });
+        
+    }]);
 
 })();
