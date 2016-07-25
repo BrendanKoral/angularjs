37aafd6aa152b694bf5ca35030466a14718333b7
Brendan Koral <brendanhkoral@gmail.com>
2016-07-25 12:47:40 -0500

    Add finished app!

--- templatestore/app/templates/templates.js
+++ templatestore/app/templates/templates.js
@@ -12,10 +12,22 @@ angular.module('templateStore.templates',['ngRoute'])
         })
 }])
 
-.controller('TemplatesCtrl', ['$scope', function ($scope) {
-    console.log($scope);
+.controller('TemplatesCtrl', ['$scope', '$http', function ($scope, $http) {
+        $http.get('json/templates.json').success(function (data) {
+            $scope.templates = data;
+    });
 }])
 
-    .controller('TemplatesDetailsCtrl', ['$scope', function ($scope) {
-    console.log($scope);
-}]);
+    .controller('TemplatesDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function ($scope, $routeParams, $http, $filter) {
+        var templateId = $routeParams.templateId;
+        $http.get('json/templates.json').success(function (data) {
+            $scope.template = $filter('filter')(data, function (d) {
+                return d.id == templateId;
+            })[0];
+            $scope.mainImage = $scope.template.images[0].name;
+        });
+        $scope.setImage = function (image) {
+            $scope.mainImage = image.name;
+
+        }
+    }]);
